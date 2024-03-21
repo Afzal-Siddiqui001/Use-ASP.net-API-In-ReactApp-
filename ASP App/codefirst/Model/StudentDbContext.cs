fc90693588cc5077ed2d8e26c5bbb33285f3bca7
Muhammad Afzal <afzal@aptech.com>
2024-03-21 13:20:57 +0500

    Examples

---
+++ ASP App/codefirst/Model/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace codefirst.Model
+{
+    
+        public class StudentDbContext : DbContext
+        {
+            public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+            {
+            }
+            public DbSet<Student> Student { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog=Dummy; User Id=sa; password=aptech; TrustServerCertificate= True");
+            }
+        }
+    }
+
